ccd5d0b0c3d84174379b9d4058eab2fb254ab210
James Cuénod <j3frea@gmail.com>
2015-09-04 16:58:27 -0500

    Initial commit

---
+++ craftbrew.js
@@ -0,0 +1,63 @@
+var timer;
+var loop;
+var timeout = 300;
+var consonants = true;
+var currentIteration = 0;
+var lastcode = 0;
+
+$(document).on("keyup", function(e) {
+    if (e.which == lastcode) {
+        loop();
+        return;
+    }
+    switch (e.which) {
+        case 8: //backspace
+            $(".content").text($(".content").text().slice(0, - 1));
+            break;
+        case 190:
+            $(".content").text($(".content").text().slice() + "ּ");
+            break;
+        case 32:
+            $(".content").text($(".content").text().slice() + " ");
+            break;
+        case 65:/* falls through */
+        case 69:/* falls through */
+        case 73:/* falls through */
+        case 79:/* falls through */
+        case 85:
+            var lastcharacterVowelish = $(".content").text().trim().slice(-1).match(/[\u05B0-\u05BB]/);
+            if ( !lastcharacterVowelish && $(".content").text().trim().length > 0) {
+                //we try for vowels - only if a vowel was hit and there's something there
+                buildLoop(hebrewVowelMap, e.which);
+                break;
+            }
+            /* falls through */
+        default:
+            buildLoop(hebrewConsonantMap, e.which);
+    }
+});
+
+function buildLoop(characterMap, keycode)
+{
+    $(".content").text($(".content").text() + characterMap[String.fromCharCode(keycode)][0]);
+    currentIteration = 0;
+    lastcode = keycode;
+    loop = function() {
+        currentIteration++;
+        index = currentIteration % (characterMap[String.fromCharCode(keycode)].length);
+        $(".content").text($(".content").text().slice(0, - 1) + characterMap[String.fromCharCode(keycode)][index]);
+        resetTimer(currentIteration);
+    };
+    resetTimer(0);
+}
+function resetTimer(multiplier)
+{
+    multiplier = multiplier > 4 ? 4 : multiplier;
+    clearTimeout(timer);
+    timer = setTimeout(endWait, timeout + 120 * multiplier);
+}
+function endWait()
+{
+    lastcode = false;
+    console.log("end");
+}

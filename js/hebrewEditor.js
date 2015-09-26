4dff2bc8fdc918a9df1f132c8c22c36981ef4c4f
James Cuénod <j3frea@gmail.com>
2015-09-25 23:55:23 -0500

    Sof pasuq keeps final character. Deleted comments.

--- js/hebrewEditor.js
+++ js/hebrewEditor.js
@@ -48,29 +48,6 @@ function HebrewEditor(editorBox) {
                    !(lastChar == "ו" && withDagesh) );
         }
     };
-    // this.endsVowelishly = function() {
-    //     var withDagesh = false;
-    //     var lastChar = this.textModel.getNthCharacter(this.textModel.getLength() - 1);
-    //     if (lastChar == "\u05BC")
-    //     {
-    //         //if last character was a dagesh, check the previous one
-    //         lastChar = this.textModel.getNthCharacter(this.textModel.getLength() - 2);
-    //         withDagesh = true;
-    //     }
-    //     /*
-    //      * \u05D0-\u05EA = UTF8 range from Aleph to Tav
-    //      *
-    //      * UTF16 characters:
-    //      * \uFB2A = HEBREW LETTER SHIN WITH SHIN DOT
-    //      * \uFB2B = HEBREW LETTER SHIN WITH SIN
-    //      *
-    //      * Note also that \uFB4B = HEBREW LETTER VAV WITH HOLAM (excluded because it's a vowel)
-    //      */
-    //     return ( this.isConsonantish(lastChar) &&
-    //             this.textModel.getCurrentText().trim().length > 0 &&
-    //             !(lastChar == "ו" && withDagesh) );
-    // };
-
     this.isConsonantish = function(char)
     {
         return char.match(/[\u05D0-\u05EA\uFB2A\uFB2B]/);
@@ -107,7 +84,7 @@ function HebrewEditor(editorBox) {
         if (t.getNthCharacter($that.getNthLastConsonantIndex(1, t)) in invertedFinalFormMap)
         {
             var charactersBetweenLastConsonants = t.getCurrentText().slice($that.getNthLastConsonantIndex(1, t), $that.getNthLastConsonantIndex(0, t));
-            if (!charactersBetweenLastConsonants.match(/[\ ־]/))
+            if (!charactersBetweenLastConsonants.match(/[\ ־׃]/))
             {
                 t.replaceNthCharacter(invertedFinalFormMap[t.getNthCharacter($that.getNthLastConsonantIndex(1, t))], $that.getNthLastConsonantIndex(1, t));
             }
